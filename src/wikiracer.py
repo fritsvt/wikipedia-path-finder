ae809f3c5cb56e7e7541acddfe17d2b68e951905
Frits <frits.vanturnhout@gmail.com>
2023-03-26 14:42:22 +0200

    add cli interface

--- src/wikiracer.py
+++ src/wikiracer.py
@@ -1,10 +1,32 @@
+import typer
+from rich.console import Console
+from rich.text import Text
+
 from service.path_finder_service import find_shortest_path
 
+
+console = Console()
+
+
+def main(first_title: str, second_title: str) -> None:
+    with console.status(
+        f"[bold green] Finding a link between {first_title} and {second_title}..."
+    ):
+        result = find_shortest_path(first_title, second_title)
+
+    for index, path in enumerate(result):
+        text = Text()
+
+        if index == 0:
+            text.append("🛫 START: ", style="bold green")
+        elif index == len(result) - 1:
+            text.append("🛬️  END: ", style="bold green")
+        else:
+            text.append("➡️ CLICK: ", style="bold magenta")
+
+        text.append(f"{path.title} ({path})")
+        console.print(text)
+
+
 if __name__ == "__main__":
-    first_title = "Apple"
-    second_title = "iPhone"
-
-    result = find_shortest_path(first_title, second_title)
-
-    for path in result:
-        print(path)
+    typer.run(main)
